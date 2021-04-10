a8851622ec4f19263e2d42f804c3f89d1e29ee08
Cody Shanahan <cody.33shan@gmail.com>
2021-04-10 12:29:23 -0700

    Added CORS and updated POST method

--- hello.py
+++ hello.py
@@ -1,14 +1,16 @@
 from flask import Flask
 from flask import request
 from flask import jsonify
+from flask_cors import CORS
 app = Flask(__name__)
+CORS(app)
 
 users = {'users_list' : [
 	{'id' : 'xyz789', 'name' : 'Charlie', 'job' : 'Janitor'},
 	{'id' : 'abc123', 'name' : 'Mac', 'job' : 'Bouncer'},
 	{'id' : 'ppp222', 'name' : 'Mac', 'job' : 'Professor'},
 	{'id' : 'yat999', 'name' : 'Dee', 'job' : 'Aspiriing Actress'},
-	{'id' : 'zap555', 'name' : 'Dennis', 'job' : 'Bartender'}]}
+	{'id' : 'zap555', 'name' : 'Denni', 'job' : 'Bartender'}]}
 
 @app.route('/')
 def hello_world():
@@ -31,6 +33,7 @@ def get_users():
 		userToAdd = request.get_json()
 		users['users_list'].append(userToAdd)
 		resp = jsonify(success=True)
+		resp.status_code = 201
 		return resp		#forgot this at first
 	elif request.method == 'DELETE':
 		userToDelete = request.get_json()
